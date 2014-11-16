911c1c94daacfc1971322737b9bc0514910a6256
zeriet <zeriet@zeriet>
2014-11-15 21:55:05 -0600

    Beans and service layers created

---
+++ src/main/java/edu/mum/cs545/service/AccountService.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.mum.cs545.service;
+
+import edu.mum.cs545.bean.*;
+
+/**
+ *
+ * @author zeriet
+ */
+public class AccountService {
+    
+}
